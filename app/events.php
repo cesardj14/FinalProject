80af7bb88fecb530d7bafb4912ed245a259ffb1f
Gustavo Charinga <gustavo_ch93@hotmail.com>
2016-09-30 03:41:31 -0400

    act 30-09-16 3:41am

--- app/events.php
+++ app/events.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class events extends Model
 {
-    //
+    protected $fillable =
+        [
+            'title', 'description', 'images', 'valid_to', 'valid_from', '', '',
+        ];
 }
